afd6a7d07b48136f83b88b07a3faaee7200c0807
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-27 16:52:54 +0000

    7 files PWA

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Assignment6-JS", {
+    scope: "/ICS20-Assignment6-JS/",
   })
 }
 
