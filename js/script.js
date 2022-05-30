9d8c8b82ad0a862db34cd2d86ccc48a806a81449
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-30 16:24:59 +0000

    2 files

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Assignment6-JS", {
